c8bbbd25eb593f2d780add3b18d33fa692acd872
jw <mohammad.sh7283@gmail.com>
2025-05-13 22:02:46 +0330

    adding new 404 and 500 error

--- blog/settings.py
+++ blog/settings.py
@@ -57,6 +57,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
